969bb6e9919da890cdc2821054e6ff98b778c86f
Anubha <anubhakabradtu@gmail.com>
2022-12-01 17:58:21 -0500

    Embeddings

--- obtain_embeddings.sh
+++ obtain_embeddings.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-OUTPUT_DIR = labse_embeddings
-rm -rf ${OUTPUT_DIR}
-mkdir -p ${OUTPUT_DIR}
+OUTPUT_DIR = "/projects/metis2/anubha/research/Multilingual-Fig-QA/translated_data_for_embeddings/embeddings"
 
 python obtain_embeddings.py \
     --model_name_or_path xlm-roberta-large \
-    --input_file translated_metaphor.tsv \
+    --input_file "translated_data_for_embeddings/translated_metaphor.tsv"\
     --max_length 128 \
     --per_device_eval_batch_size 32 \
-    --output_dir ${OUTPUT_DIR} \
+    --output_dir "/projects/metis2/anubha/research/Multilingual-Fig-QA/translated_data_for_embeddings/embeddings" \
     --seed 0 \
     --use_labse
